088de6f592732cee14b2586ce63d852fda541bfb
pammelavicenti <162892438+pammelavicenti@users.noreply.github.com>
2024-03-11 09:50:05 -0300

    editada

---
+++ exercicio_02.py
@@ -0,0 +1,12 @@
+#METADE
+#faca um procedimento que leia 10 numeros digitados pelo usuario, armazene a metade de cada um deles numa lista, e depois imprima esta lista.
+
+def metade():
+    l = []
+
+    for i in range (10):
+        n = float(input("digite um numero: "))
+        l.append(n/2)
+    for n in l:
+        print(n)
+metade()
