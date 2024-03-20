1434015aef37ed82603a2b6db969d96e2c68da00
pammelavicenti <pammelapvr@gmail.com>
2024-03-20 10:49:16 -0300

    Update LISTAS.py

--- LISTAS.py
+++ LISTAS.py
@@ -1,71 +1,79 @@
 #1-Regressiva: Faca uma funcao que crie e retorne uma lista com todos os numeros pares de 1 a 100, porem na ordem regressiva.
+def main():   
+    def pares():
+        i = 100
+        l = []
+
+        while i >= 1:
+            if i%2 == 0:
+                l.append(i)
+            i = i-1
+        return l
     
-def pares():
-    i = 100
-    l = []
+    def pares2():
+        i = 100
+        l = []
 
-    while i >= 1:
-        if i%2 == 0:
+        while i >= 2:  # Alteração aqui: iterando até 2 com passo -2
             l.append(i)
-        i = i-1
-    return l
-def pares2():
-    i = 100
-    l = []
+            i -= 2  # Alteração aqui: passo -2 para encontrar números pares em ordem regressiva
+        return l
+    
 
-    while i >= 2:
-        l.append(i)
-        i = i-2
-    return l 
-print(pares())
-print(pares2())
+    #2-METADE
+    #faca um procedimento que leia 10 numeros digitados pelo usuario, armazene a metade de cada um deles numa lista, e depois imprima esta lista.
 
+    def metade():
+        l = []
 
-#2-METADE
-#faca um procedimento que leia 10 numeros digitados pelo usuario, armazene a metade de cada um deles numa lista, e depois imprima esta lista.
+        for i in range (10):
+            n = float(input("digite um numero: "))
+            l.append(n/2)
+        for n in l:
+            print(n)
+    
 
-def metade():
-    l = []
+    #3-LEITURA
+    #Dado um numero N, faça uma fanção que leia N números inteiros e retorne uma lista com esses números.
 
-    for i in range (10):
-        n = float(input("digite um numero: "))
-        l.append(n/2)
-    for n in l:
-        print(n)
-metade()
+    def n_numeros(n):
+        i = 1
+        l = []
 
-#3-LEITURA
-#Dado um numero N, faça uma fanção que leia N números inteiros e retorne uma lista com esses números.
+        for i in range(n):
+            x = int(input("Digite um numero: "))
+            l.append(x)
+        
 
-def n_numeros(n):
-    i = 0
-    l = []
+        return l
+    
 
-    while i < n:
-        x = int(input("Digite um numero: "))
-        l.append(x)
-        i += 1
+    #4- OCORRENCIAS
+    #Dada uma lista e um elemento, retorne o numero de ocorrencias do elemento na lista.
 
-    return 1
-n = int(input("Digite a quantidade de numeros: "))
-print(n_numeros(n))
+    def ocorrencias (x, l):
+        n = 0 
 
-#4- OCORRENCIAS
-#Dada uma lista e um elemento, retorne o numero de ocorrencias do elemento na lista.
+        for elem in l:
+            if elem == x:
+                n += 1 
+        return n 
+    
 
-def ocorrencias (x, l):
-    n = 0 
-
-    for elem in l:
-        if elem == x:
-            n += 1 
-    return n 
-l = [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 4, 3, 3, 3, 2, 1]
-print(ocorrencias (3,l))
-
-#5-MAXIMO 
-#Dada uma lista de numeros, faça uma função que encontre e retorne o maior deles.
-
-
+    #5-MAXIMO 
+    #Dada uma lista de numeros, faça uma função que encontre e retorne o maior deles.
+    # Chamando as funções
+    print("Números pares de 1 a 100 em ordem regressiva:", pares())
+    print("Números pares de 1 a 100 em ordem regressiva:", pares2())
+    
+    print("Metades dos números digitados:")
+    metade()
+    n = int(input("Digite a quantidade de numeros para leitura: "))
+    print("Lista de números lida:", n_numeros(n))
+    
+    l = [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 4, 3, 3, 3, 2, 1]
+    print("Número de ocorrências de '3' na lista:", ocorrencias(3, l))
+    
+main()
 
     
